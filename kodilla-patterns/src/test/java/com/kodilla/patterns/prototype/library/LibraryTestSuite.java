da52a68860b49a422ca2e2c16958bfd8c5180371
Kacper Koralewski <koralex666@gmail.com>
2022-11-05 14:03:35 +0100

    11.2 prototype

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class LibraryTestSuite {
 
     @Test
-    void testGetBooks(){
+    void testGetBooksCleaning(){
 
         //given
         //creating the first Library
@@ -21,6 +21,7 @@ public class LibraryTestSuite {
                 .forEach(n -> library.getBooks().add(new Book("King Arthur "+n,
                                                     "J. R. R. Tolkien",
                                                     LocalDate.of(1962+n, n*2, 7+n*3-n*2))));
+
 
         //making a shallow copy of object library
         Library swallowCopy = null;
@@ -43,7 +44,6 @@ public class LibraryTestSuite {
         //When - cleaning Library
         library.getBooks().clear();
 
-
         //Then
         System.out.println(library);
         System.out.println(swallowCopy);
@@ -53,17 +53,52 @@ public class LibraryTestSuite {
         assertEquals(5, deepCopy.getBooks().size());
         assertEquals(swallowCopy.getBooks(), library.getBooks());
         assertNotEquals(deepCopy.getBooks(), library.getBooks());
+    }
+
+    @Test
+    void testGetBooks(){
+
+        //given
+        //creating the first Library
+        Library library = new Library("TheOne");
+        IntStream.iterate(1, n -> n + 1)
+                .limit(5)
+                .forEach(n -> library.getBooks().add(new Book("King Arthur "+n,
+                                                    "J. R. R. Tolkien",
+                                                    LocalDate.of(1962+n, n*2, 7+n*3-n*2))));
 
 
+        //making a shallow copy of object library
+        Library swallowCopy = null;
+        try {
+            swallowCopy = library.shallowCopy();
+            swallowCopy.setName("Shallow Copy");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //making a deep copy of object library
+        Library deepCopy = null;
+        try {
+            deepCopy = library.deepCopy();
+            deepCopy.setName("Deep Copy");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //When - cleaning Library
+        //System.out.println(library.getBooks().contains(new Book("King Arthur 1", "J. R. R. Tolkien", LocalDate.of(1963, 2, 8))));
+        library.getBooks().remove(new Book("King Arthur 1", "J. R. R. Tolkien", LocalDate.of(1963, 2, 8)));
 
 
-
-
-
-
-
-
-
-
+        //Then
+        System.out.println(library);
+        System.out.println(swallowCopy);
+        System.out.println(deepCopy);
+        assertEquals(4, library.getBooks().size());
+        assertEquals(4, swallowCopy.getBooks().size());
+        assertEquals(5, deepCopy.getBooks().size());
+        assertEquals(swallowCopy.getBooks(), library.getBooks());
+        assertNotEquals(deepCopy.getBooks(), library.getBooks());
     }
 }
